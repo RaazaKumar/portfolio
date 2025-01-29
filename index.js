56081ee1d2dd27b418a0338a1e8ca82918f4cac6
raja <rajput4868738@gmail.com>
2025-01-29 11:51:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector("#btn");
+const dark = document.querySelectorAll(".dark");
+// const light = document.querySelector(".light");
+
+
+   
+    dark.forEach(function(e){
+      e.addEventListener("click",function(n){
+        n.
+      })
+    });
